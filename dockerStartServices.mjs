870e4aa6d36a481dd5fcfa47ad45cf93f2bbe556
Brycen G <brycengranville@outlook.com>
2023-02-10 01:57:32 -0500

    ci(actions): fix cypress ci integration and add semantic prs (#15)
    * fix(actions): set cypress working directory
    
    * ci(actions): fix e2e testing
    
    * ci(actions): cache cypress binaries
    
    * ci(actions): cache cypress binaries
    
    * chore(security): do not output process.env
    
    * chore(repo): do not upload jest
    
    * chore(repo): remove jest coverage folder
    
    * ci(actions): setup backend environment variables
    
    * ci(actions): add needed prereqs for frontend ci
    
    * chore(repo): mark package as private
    
    * ci(actions): fix backend redis container
    
    * ci(actions): move redis container to backend
    
    * ci(actions): add back redis container to frontend
    
    * ci(actions): finally fix frontend builds
    
    * ci(actions): actually install cypress
    
    Finally got a something working locally for monorepos
    
    * ci(actions): add semantic prs
    
    * ci(actions): cancel previous runs
    
    * ci(actions): replace cypress action with bash solution
    
    this change is temporary until i can fix builiding

---
+++ dockerStartServices.mjs
@@ -0,0 +1,9 @@
+import {execa} from 'execa';
+
+if (process.env.CI) {
+  console.log('Not starting any Docker containers, CI mode is activated.')
+}
+else {
+  execa('docker', ['compose', 'up', 'cache', '-d'])
+  .stderr.pipe(process.stderr)
+}
