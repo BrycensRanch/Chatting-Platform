be041a3d1f35505c17fa2b226222808dd63df900
Nicholas Berlette <nick@berlette.com>
2023-02-12 10:32:38 -0800

    chore(repo): commit signing

--- dockerStartServices.mjs
+++ dockerStartServices.mjs
@@ -1,7 +1,6 @@
 import {execa} from 'execa';
 
 
-// For more consistent behavior, our CI starts a Redis container independently of our Docker-Compose
 if (process.env.CI) {
   console.log('Not starting any Docker containers, CI mode is activated.')
 }
