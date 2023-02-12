1e152e7bcc87f989f8286d54ed83722b23efbd95
Nicholas Berlette <nick@berlette.com>
2023-02-12 08:23:34 -0800

    chore(repo): remove unnecessary comment
    the file is self explanatory

--- dockerStartServices.mjs
+++ dockerStartServices.mjs
@@ -1,7 +1,5 @@
 import {execa} from 'execa';
 
-
-// For more consistent behavior, our CI starts a Redis container independently of our Docker-Compose
 if (process.env.CI) {
   console.log('Not starting any Docker containers, CI mode is activated.')
 }
