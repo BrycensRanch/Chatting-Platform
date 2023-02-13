8ca8b020c581c93b4f067b7ac39d0682c2a395e0
BrycensRanch <brycengranville@outlook.com>
2023-02-13 15:38:32 -0500

    feat(docker): monorepo dockerfile

--- dockerStartServices.mjs
+++ dockerStartServices.mjs
@@ -1,10 +1,12 @@
-import {execa} from 'execa';
 
 
 if (process.env.CI) {
   console.log('Not starting any Docker containers, CI mode is activated.')
 }
 else {
-  execa('docker', ['compose', 'up', 'cache', '-d'])
-  .stderr.pipe(process.stderr)
+  import('execa')
+  .then((module) => {
+    module.execa('docker', ['compose', 'up', 'cache', '-d'])
+    .stderr.pipe(process.stderr)
+  })
 }
